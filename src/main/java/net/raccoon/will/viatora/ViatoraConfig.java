635494aebacf0fecd26473f20452503ae4410c36
axtrough <lolmyfriend14@gmail.com>
2025-05-15 23:51:21 +0200

    sorry bert. changed my mind

--- src/main/java/net/raccoon/will/viatora/ViatoraConfig.java
+++ src/main/java/net/raccoon/will/viatora/ViatoraConfig.java
@@ -9,7 +9,6 @@ public class ViatoraConfig {
     public static final ModConfigSpec SPEC;
     public static final ModConfigSpec.EnumValue<TrampleEnum> TYPE;
     public static final ModConfigSpec.IntValue FEATHER_FALLING_LEVEL;
-    public static final ModConfigSpec.BooleanValue VANILLA_STYLED_DYED_BEEHIVES;
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     static {
@@ -19,11 +18,8 @@ public class ViatoraConfig {
                 .defineEnum("type", TrampleEnum.FEATHER_FALLING,
                         Arrays.asList(TrampleEnum.values()));
 
-        FEATHER_FALLING_LEVEL = BUILDER.comment("Min Feather Falling level required to be unable to trample")
+        FEATHER_FALLING_LEVEL = BUILDER.comment("Feather falling level required to trample")
                 .defineInRange("featherFallingLevel", 1, 1, 4);
-
-        VANILLA_STYLED_DYED_BEEHIVES = BUILDER.comment("Use vanilla-style textures for dyed beehives (true = vanilla-styled, false = custom)")
-                .define("vanillaStyledDyedBeehives", false);
 
         SPEC = BUILDER.build();
     }
